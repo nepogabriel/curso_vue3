91802d17dcab5d8d7a6a1e694649a3bc8d40090f
Gabriel Ribeiro <gabrielribeiroonline@gmail.com>
2022-07-22 00:42:29 -0300

    Utilizando evento de submit e como preparar os dados para enviar para API

--- 2_projeto_vue/src/components/Form.vue
+++ 2_projeto_vue/src/components/Form.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
     <h2>Formulário:</h2>
-    <form action="">
-      <InputText/>
-      <InputText/>
+    <!-- $event - Eventos da função nativa do vue.js 3 (javascript)-->
+    <form action="" @submit="enviarFormulario($event)">
+      <div>
+        <input type="text" v-model="name">
+      </div>
+
+      <div>
+        <input type="text" v-model="email">
+      </div>
+
       <Submit/>
     </form>
   </div>
@@ -18,6 +25,32 @@
     components: {
       InputText,
       Submit
+    },
+    data() {
+      return {
+        name: '',
+        email: ''
+      }
+    },
+    methods: {
+      enviarFormulario(e) {
+        e.preventDefault();
+
+        const name = this.name;
+        const email = this.email;
+
+        console.log('Formulário enviado!');
+        console.log(`Meu nome é ${name}`);
+        console.log(`Meu e-mail é ${email}`);
+
+        // Aqui seria a requisição Ajax para enviar esses dados para a API
+      }
     }
   }
 </script>
+
+<style>
+p {
+  margin: 50px;
+}
+</style>
