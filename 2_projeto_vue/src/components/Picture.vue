e29a07d62405685ab6bf0b4c44daa34f474c28d1
Gabriel Ribeiro <gabrielribeiroonline@gmail.com>
2022-07-22 12:39:02 -0300

    Reutilização de componentes vue

--- 2_projeto_vue/src/components/Picture.vue
+++ 2_projeto_vue/src/components/Picture.vue
@@ -1,11 +1,17 @@
 <template>
   <!-- v-bind: e : são a mesma coisa -->
   <img :src="avatar" v-bind:alt="descricao">
+  <Reutilizacao/>
 </template>
 
 <script>
+  import Reutilizacao from "@/components/Reutilizacao";
+
   export default {
     name: 'Picture',
+    components: {
+      Reutilizacao
+    },
     data() {
       return {
         avatar: '/img/avatar.png', // Sempre que estiver acessando do / significa que está entrando no public
--- 2_projeto_vue/src/components/PrimeiroComponente.vue
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -6,16 +6,22 @@
     <p>Minha profissão é {{ profession }}</p>
 
     <MultiplosEventos/>
+    <Reutilizacao/>
+    <Reutilizacao/>
+    <Reutilizacao/>
+    <Reutilizacao/>
   </div>
 </template>
 
 <script>
   import MultiplosEventos from "@/components/MultiplosEventos";
+  import Reutilizacao from "@/components/Reutilizacao";
 
   export default {
     name: 'PrimeiroComponente', // Nome do componente
     components: {
-      MultiplosEventos
+      MultiplosEventos,
+      Reutilizacao
     },
     data() { // Utilizando dados no componente (esses dados só podem ser usados dentro do componente)
       return {
