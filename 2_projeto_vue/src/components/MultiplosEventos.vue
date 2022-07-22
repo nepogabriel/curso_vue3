9f7ae785c4acf1dc465df75ed38e1138b2009c4c
Gabriel Ribeiro <gabrielribeiroonline@gmail.com>
2022-07-22 01:07:01 -0300

    Aprendendo o funcionamento de múltiplos eventos

---
+++ 2_projeto_vue/src/components/MultiplosEventos.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <!-- É recomendado que os parâmetros devem ser dados dinâmicos (variável, array, objetos) -->
+    <button @click="primeiro('Primeiro evento', $event), segundo(txt, $event), terceiro()">
+      Ativar múltiplos eventos
+    </button>
+    <p>{{ multiplos_eventos }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MultiplosEventos',
+  data() {
+    return {
+      multiplos_eventos: '',
+      txt: 'Segundo evento'
+    }
+  },
+  methods: {
+    primeiro(text, e) {
+      this.multiplos_eventos = `${text} disparado!`;
+    },
+    segundo(text, e) {
+      setTimeout(() => this.multiplos_eventos = `${text} disparado!`, 500);
+    },
+    terceiro() {
+      setTimeout(() => this.multiplos_eventos = '', 1500);
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  margin: 100px;
+}
+</style>
--- 2_projeto_vue/src/components/PrimeiroComponente.vue
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -4,12 +4,19 @@
     <h1>Olá Vue!</h1>
     <p>Meu nome é {{ name }}</p>
     <p>Minha profissão é {{ profession }}</p>
+
+    <MultiplosEventos/>
   </div>
 </template>
 
 <script>
+  import MultiplosEventos from "@/components/MultiplosEventos";
+
   export default {
     name: 'PrimeiroComponente', // Nome do componente
+    components: {
+      MultiplosEventos
+    },
     data() { // Utilizando dados no componente (esses dados só podem ser usados dentro do componente)
       return {
         name: 'Gabriel',
