b56f402fd36ac941664f0f0f2c847cc0ae08a8de
Gabriel Ribeiro <gabrielribeiroonline@gmail.com>
2022-08-04 21:26:32 -0300

    Tela de dashboard e lista dinâmica de pedidos feitos

--- 3_make_your_burguer/src/views/PedidosView.vue
+++ 3_make_your_burguer/src/views/PedidosView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="main-container">
-    <h1 class="page-title">PEDIDOS</h1>
+    <h1 class="page-title">Gerenciar Pedidos</h1>
+    <DashboardComponent/>
   </div>
 </template>
+
+<script>
+import DashboardComponent from "@/components/DashboardComponent";
+
+export default {
+  name: 'PedidosView',
+  components: {
+    DashboardComponent
+  }
+}
+</script>
